202e56adace9aaac18091712f5dc93e6ad496a8a
youssef-ouftallah-vinci <youssef.ouftallah@student.vinci.be>
2023-10-03 23:23:05 +0200

    1.2 : app middleware

---
+++ exercices/1.2/app.js
@@ -0,0 +1,27 @@
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+
+var app = express();
+
+let nombre = 0;
+app.use((req, res, next) => {
+    if(req.method == 'GET') nombre++;
+    console.log('GET counter :', nombre);
+    next();
+});
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+module.exports = app;
