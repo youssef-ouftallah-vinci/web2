624a714bba5ce4a8ac4b59ee2c10d002f2264c1c
youssef-ouftallah-vinci <youssef.ouftallah@student.vinci.be>
2023-10-08 19:59:03 +0200

    1.8 : API : Linter & formatter

---
+++ exercices/1.8/app.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+
+const usersRouter = require('./routes/users');
+const filmsRouter = require('./routes/films');
+
+const app = express();
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+app.use('/users', usersRouter);
+app.use('/films', filmsRouter);
+
+module.exports = app;
