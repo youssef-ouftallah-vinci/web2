63650c2e2838754ac15abd219c291daebaa27fe0
youssef-ouftallah-vinci <youssef.ouftallah@student.vinci.be>
2023-10-14 17:00:17 +0200

    2.3.1 : event object & form

---
+++ exercices/2.3.1/index.js
@@ -0,0 +1,9 @@
+const form = document.querySelector('form');
+const wish = document.querySelector('#wish');
+const messageDiv = document.querySelector('#message');
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  form.style.display = 'none';
+  messageDiv.innerText = `Ton voeu est : ${wish.value}`;
+});
