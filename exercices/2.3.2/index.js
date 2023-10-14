79e306bedd4edd3c70cfb4fe5631d75755537692
youssef-ouftallah-vinci <youssef.ouftallah@student.vinci.be>
2023-10-14 17:30:22 +0200

    2.3.2 : event object & multiple divs

---
+++ exercices/2.3.2/index.js
@@ -0,0 +1,12 @@
+const divs = document.querySelectorAll(".color-div");
+
+divs.forEach((div) => {
+  div.addEventListener('click', (e) => {
+    e.target.innerText = e.target.style.backgroundColor;
+    e.target.style.width = "100px";
+    e.target.style.height = "100px";
+  });
+});
+
+
+
